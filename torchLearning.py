d3246b83554e83a8a91d781dbe7bb644a91b3f92
AKAZhiyu <110835597+AKAZhiyu@users.noreply.github.com>
2024-08-01 09:26:42 +0800

    Add early stopping
    Implement the early stopping class for pytorch

--- torchLearning.py
+++ torchLearning.py
@@ -1442,3 +1442,29 @@ def plotsample(data):
         axs[i].imshow(np.transpose(npimg, (1, 2, 0))) 
         axs[i].set_title(nplabel) #给每个子图加上标签
         axs[i].axis("off") #消除每个子图的坐标轴
+
+class EarlyStopping():
+    def __init__(self, patience = 5, tol = 0.0005): #惯例地定义我们所需要的一切变量/属性\
+        #当连续patience次迭代时，这一轮迭代的损失与历史最低损失之间的差值小于阈值时
+        #就触发提前停止
+        
+        self.patience = patience
+        self.tol = tol #tolerance，累积5次都低于tol才会触发停止
+        self.counter = 0 #计数，计算现在已经累积了counter次
+        self.lowest_loss = None
+        self.early_stop = False #True - 提前停止，False - 不要提前停止
+    
+    def __call__(self,val_loss):
+        if self.lowest_loss == None: #这是第一轮迭代
+            self.lowest_loss = val_loss
+        elif self.lowest_loss - val_loss > self.tol:
+            self.lowest_loss = val_loss
+            self.counter = 0
+        elif self.lowest_loss - val_loss < self.tol:
+            self.counter += 1
+            print("\t NOTICE: Early stopping counter {} of {}".format(self.counter,self.patience))
+            if self.counter >= self.patience:
+                print('\t NOTICE: Early Stopping Actived')
+                self.early_stop = True
+        return self.early_stop
+        #这一轮迭代的损失与历史最低损失之间的差 - 阈值
